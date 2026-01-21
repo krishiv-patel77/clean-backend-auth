46b6f2113c07e407e6229c8675d61e9b821f807a
reaper-debug <krishivpatel067@gmail.com>
2026-01-21 13:17:54 -0500

    Fixed user router bugs. Router now works as intended.

--- backend/src/users/schemas.py
+++ backend/src/users/schemas.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 from datetime import datetime 
 
 class UserUpdateRequest(BaseModel):
     """ This is the request model for Put requests so it must have all data avaliable """
-    first_name: Optional[str]
-    last_name: Optional[str]
-    email: Optional[str]
-    university: Optional[str]
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    university: Optional[str] = None
 
 class CurrentUserResponse(BaseModel):
     first_name: str
@@ -15,6 +15,8 @@ class CurrentUserResponse(BaseModel):
     email: str
     university: Optional[str]      # University is an optional field
     time_created: datetime
+
+    model_config = ConfigDict(from_attributes=True)
 
 class ChangePasswordRequest(BaseModel):
     current_password: str
